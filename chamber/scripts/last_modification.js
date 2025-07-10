5f4cae2bb640710cd17f90f294d6ed86f7cf42d8
ToriLutz <vglutz@comcast.net>
2025-07-10 11:34:20 -0700

    started chamber of commerce home

---
+++ chamber/scripts/last_modification.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+let year = document.getElementById("currentYear");
+year.innerHTML = currentYear;
+const modifiedYear = document.lastModified;
+document.getElementById('lastModified').innerText = modifiedYear;
