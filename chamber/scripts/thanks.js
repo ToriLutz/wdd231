19d8025550d839fca6cf850375c0d26e00e20e7f
ToriLutz <vglutz@comcast.net>
2025-07-25 16:28:51 -0700

    update

--- chamber/scripts/thanks.js
+++ chamber/scripts/thanks.js
@@ -4,11 +4,11 @@ console.log(getString);
 const myInfo = new URLSearchParams(getString);
 console.log(myInfo);
 
-console.log(myInfo.get('first'));
+console.log(myInfo.get('fname'));
 
-
-document.querySelector('#results'.innerHTML = `<p>Thank you
-    ${myInfo.get('first')} ${myInfo.get('last')}</p>
-    <p> we will be in contact to discuss details on your ${myInfo.get('membership')}</p>
-    <p> for your business ${myInfo.get('business')} we will reach out via your number: ${myInfo.get('phone')}</p>
-    <p> and Email: ${myInfo.get('email')} to confirm your details`)
+document.querySelector('#results').innerHTML = `
+  <p>Thank you ${myInfo.get('fname')} ${myInfo.get('lname')}</p>
+  <p>We will be in contact to discuss details on your ${myInfo.get('membership')}</p>
+  <p>For your business ${myInfo.get('bname')}, we will reach out via your number: ${myInfo.get('phone')}</p>
+  <p>And email: ${myInfo.get('email')} to confirm your details.</p>
+`;
