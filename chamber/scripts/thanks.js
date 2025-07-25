c557a1690bddcbb4df9340c030ca44b2b981e589
ToriLutz <vglutz@comcast.net>
2025-07-25 16:21:15 -0700

    update

--- chamber/scripts/thanks.js
+++ chamber/scripts/thanks.js
@@ -7,9 +7,8 @@ console.log(myInfo);
 console.log(myInfo.get('first'));
 
 
-document.querySelector('#results'.innerHTML = `<p>Appointment for
+document.querySelector('#results'.innerHTML = `<p>Thank you
     ${myInfo.get('first')} ${myInfo.get('last')}</p>
-    <p>${myInfo.get('ordinance')}</p>
-    <p> on ${myInfo.get('date')} in the ${myInfo.get('location')} temple
-    <p> Your Number : ${myInfo.get('phone')} </p> 
-    <p> Your Email: ${myInfo.get('email')}</p>`)
+    <p> we will be in contact to discuss details on your ${myInfo.get('membership')}</p>
+    <p> for your business ${myInfo.get('business')} we will reach out via your number: ${myInfo.get('phone')}</p>
+    <p> and Email: ${myInfo.get('email')} to confirm your details`)
