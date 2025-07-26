e049c04e794afadaed5474dcd426ce3eea3aae73
ToriLutz <vglutz@comcast.net>
2025-07-25 17:16:53 -0700

    Update buttons.js

--- chamber/scripts/buttons.js
+++ chamber/scripts/buttons.js
@@ -3,26 +3,30 @@ const memberships = [
   {
     level: "Non Profit",
     value: 0,
+    Price: '$0',
     description:
       "This membership is for the non profits, it is free and will get you on the directory page ",
   },
   {
     level: "Bronze",
     value: 1,
+    Price: '$25',
     description:
-      "This membership is the first level and gets you on the directory page.",
+      "This membership is the first level and gets you on the directory page and access to special events.",
   },
   {
     level: "Silver",
     value: 2,
+    Price: '$50',
     description:
-      "This membership is the first level and gets you on the directory page and may get you on the home page",
+      "This membership is the first level and gets you on the directory page and may get you on the home page access to special events and advertising",
   },
   {
     level: "Gold",
     value: 3,
+    Price: '$100',
     description:
-      "This membership is the first level and gets you on the directory page and WILL get you on the home page",
+      "This membership is the first level and gets you on the directory page and WILL get you on the home page access special events, training, advertising",
   },
 ];
 
@@ -39,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
     membershipDiv.setAttribute("member-credits", member.value);
 
     const courseTitle = document.createElement("h3");
-    courseTitle.textContent = `${member.level} ${member.value}`;
+    courseTitle.textContent = `${member.level}`;
     membershipDiv.appendChild(courseTitle);
 
     // Show details on click
@@ -54,7 +58,8 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!courseDetails) return; // Ensure the element exists
     courseDetails.innerHTML = `
       <button id="closeModal">X</button>
-      <h2>${member.level} ${member.value}</h2>
+      <h2>${member.level}</h2>
+      <h3>Monthly Membership: ${member.Price}</h3>
       <p>${member.description}</p>
     `;
     courseDetails.showModal();
