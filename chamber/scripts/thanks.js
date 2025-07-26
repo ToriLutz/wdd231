f2ef6603b90f188dd1a36c215db4631b3692eb2c
ToriLutz <vglutz@comcast.net>
2025-07-25 17:19:57 -0700

    updating the thankyou page

--- chamber/scripts/thanks.js
+++ chamber/scripts/thanks.js
@@ -7,8 +7,9 @@ console.log(myInfo);
 console.log(myInfo.get('fname'));
 
 document.querySelector('#results').innerHTML = `
-  <p>Thank you ${myInfo.get('fname')} ${myInfo.get('lname')}</p>
-  <p>We will be in contact to discuss details on your ${myInfo.get('membership')}</p>
+  <h3>Thank you ${myInfo.get('fname')} ${myInfo.get('lname')}</h3>
+  <h4>We will be in contact to discuss details on your ${myInfo.get('membership')}</h4>
   <p>For your business ${myInfo.get('bname')}, we will reach out via your number: ${myInfo.get('phone')}</p>
   <p>And email: ${myInfo.get('email')} to confirm your details.</p>
+  <p>${myInfo.get('formLoadTimestamp')}</p>
 `;
