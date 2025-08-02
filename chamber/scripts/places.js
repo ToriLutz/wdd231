c04165e9c490cc557bc9b4c4f877e6c62d6975b1
ToriLutz <vglutz@comcast.net>
2025-08-02 16:33:33 -0700

    updates

--- chamber/scripts/places.js
+++ chamber/scripts/places.js
@@ -1,20 +1,24 @@
-import places from './data/placeData.js'; 
+import {places} from "./data/placeData.js";
 const cards = document.getElementById("place"); 
 
 
 const displayPlaces = (places) => {
+  console.log("Displaying places:", places);
   places.forEach((place) => {
     const card = document.createElement("div");
+    card.classList.add("places-card", "padding-none", "grid");
     const businessCardHeader = document.createElement("div");
     const name = document.createElement("h2");
+    name.classList.add("head-font");
     name.textContent = place.name; 
 
     const theAddress = document.createElement("address");
     theAddress.innerText = place.address;
+    theAddress.classList.add("body-font");
 
     const description = document.createElement("p");
     description.innerText = place.description;
-
+    description.classList.add("body-font");
     const imageurl = document.createElement("img");
     imageurl.setAttribute("src", place.photoUrl); 
     imageurl.setAttribute("alt", `Image of ${place.name}`);
