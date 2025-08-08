d6a303f41dbbd6be88893c6536b2472a2e5665a9
ToriLutz <vglutz@comcast.net>
2025-08-08 14:37:55 -0700

    script

--- project/scripts/memberships.js
+++ project/scripts/memberships.js
@@ -16,11 +16,40 @@ const membership = [
     }
 ];
 
-document.addEventListener("DOMContentLoaded", () =>{const courseDetails = document.getElementById("memberships-details");
+document.addEventListener("DOMContentLoaded", () =>{const memberDetails = document.getElementById("memberships-details");
   const container = document.querySelector(".box-modals");
   container.innerHTML = "";
 
+membership.forEach((member) =>{
+    const membershipDiv = document.createElement("div");
+    membershipDiv.classList.add("membership");
+    membershipDiv.setAttribute("member-level", member.level);
+    membershipDiv.setAttribute("member-credits", member.value);
 
+    const memberTitle = document.createElement("h3");
+    memberTitle.textContent = `${member.level}`;
+    membershipDiv.appendChild(memberTitle);
 
+      membershipDiv.addEventListener("click", () => {
+      displayCourseDetails(member);
+    });
+
+    container.appendChild(membershipDiv);
+  });
+
+  function displayCourseDetails(member) {
+    if (!memberDetails) return; // Ensure the element exists
+    memberDetails.innerHTML = `
+      <button id="closeModal">X</button>
+      <h2 class = "head-font">${member.level}</h2>
+      <h3 class = "head-font">Monthly Membership: ${member.Price}</h3>
+      <p class = "body-font">${member.Benefits}</p>
+    `;
+    memberDetails.showModal();
+
+    document.getElementById("closeModal").addEventListener("click", () => {
+      memberDetails.close();
+    });
+  }
 });
 
